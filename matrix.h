2814b66e8849dd880baa21559f0439a9186e6753
tyom <60318359+ttyomaaa@users.noreply.github.com>
2020-04-23 23:37:29 +0300

    jordan's method(main element of the whole matrix)
    N_MAX and M_MAX might be deleted

---
+++ matrix.h
@@ -0,0 +1,13 @@
+int read_matrix(double *a, int n, const char *name);
+void print_matrix(double *a, int m, int l, int n);
+void init_matrix(double *a, int n, int k);
+void init_b(double *a, double *b, int n);
+double residual(double *a, double *b, double *x, int n);
+double accuracy(double *x, int n);
+
+#define L_MAX 10
+#define N_MAX 10
+
+#define SUCCESS 1
+#define ERROR_READ -1
+#define ERROR_OPEN -2
